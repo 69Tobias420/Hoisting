334aeeccffe78fa15eb47b8201eeb097755b62ee
Isabell Kiki Dylmer <89775022+KikiAquaFox@users.noreply.github.com>
2022-02-03 13:11:47 +0100

    Update app.js

--- Hoisting/assets/js/app.js
+++ Hoisting/assets/js/app.js
@@ -10,3 +10,12 @@ function myNeighbors(rightN, leftN) {
   console.log("My right neighbors are " + rightN)
   console.log("My left neighbors are " + leftN)
 }
+
+//--------| Example number Two (2)
+console.log("-------Example 2-------")
+
+favAnimal("Female", "Lynx")
+
+function favAnimal(animalGender, animalName) {
+  console.log("My favorite animal is " + animalGender + " " + animalName)
+}
